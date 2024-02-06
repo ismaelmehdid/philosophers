51d3556a92ec4c884a6ca2b5d83b0ac62040c090
ismaelmehdid <ismaelmehdidwork@gmail.com>
2024-02-06 17:35:41 +0100

    base of the project

---
+++ philo/philosophers.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismaelmehdid <ismaelmehdid@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 15:07:22 by ismaelmehdi       #+#    #+#             */
+/*   Updated: 2024/02/06 17:35:08 by ismaelmehdi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 5 || argc > 6)
+	{
+		write (2, "Error, Wrong number of arguments\n", 34);
+		return (1);
+	}
+	if (argc == 5)
+	{
+		
+	}
+	else if (argc == 6)
+	{
+		
+	}
+}
---
+++ philo/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismaelmehdid <ismaelmehdid@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 15:07:32 by ismaelmehdi       #+#    #+#             */
+/*   Updated: 2024/02/06 16:35:28 by ismaelmehdi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <pthread.h>
+
+
+
+#endif
